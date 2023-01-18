1740c4ea02c8c561f1c9b0346546d15a8dd13756
Raj Shekhar <channiecx954@gmail.com>
2023-01-19 02:46:52 +0530

    Add 1/3 sections of landing page

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Avatar from './Avatar.svelte';
+</script>
+
+<nav class="fixed top-0 w-screen bg-white text-blac flex items-center justify-between px-10">
+	<header class="font-bold text-2xl">Serenity</header>
+	<ul class="flex gap-4">
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li><a href="/journals">My Journals</a></li>
+		<li>
+			<a href="/self-care">Self Care</a>
+		</li>
+		<li><a href="/tips">Tips</a></li>
+	</ul>
+
+	<Avatar />
+</nav>
+
+<style>
+	nav {
+		height: 15vh;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,61 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Navbar from '../components/Navbar.svelte';
+	import { gsap } from 'gsap';
+	import { ScrollTrigger } from 'gsap/ScrollTrigger';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		gsap.registerPlugin(ScrollTrigger);
+
+		const tl = gsap.timeline();
+
+		tl.from('section header', {
+			scrollTrigger: 'section header',
+			opacity: 0,
+			duration: 2
+		})
+			.from('img', {
+				y: 500,
+				duration: 1
+			})
+			.from('#section1_desc', {
+				opacity: 0,
+				duration: 1
+			});
+	});
+</script>
+
+<Navbar />
+
+<main class="h-screen w-screen">
+	<!-- First section  -->
+	<section class="flex flex-col min-h-full justify-between bg-orange-50">
+		<header class="text-center">Healing minds, one day at a time</header>
+
+		<div class="flex w-full h-full justify-between">
+			<div class="w-1/3 h-1/3 max-w-6xl">
+				<img src="images/irritated.webp" alt="Irritated person" />
+			</div>
+
+			<div id="section1_desc" class="flex flex-col justify-center gap-10">
+				<p class="max-w-xl">Having trouble dealing with the stress and anxiety in your life?</p>
+			</div>
+		</div>
+	</section>
+</main>
+
+<style>
+	section {
+		padding: 17vh;
+	}
+
+	section > header {
+		font-family: 'Livvic';
+		font-size: 4rem;
+	}
+
+	#section1_desc {
+		font-family: 'Livvic';
+		font-size: 2rem;
+	}
+</style>
